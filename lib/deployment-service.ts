5372c65baac3d07e5d57eba69e8bc6e84bcb386a
God is a girl <chenyun.qian@outlook.com>
2025-04-12 15:11:14 +0800

    feat: update cdk template content

--- lib/deployment-service.ts
+++ lib/deployment-service.ts
@@ -6,6 +6,9 @@ import {
   CfnOutput,
   RemovalPolicy,
 } from "aws-cdk-lib";
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from "constructs";
 
 /*
@@ -21,6 +24,7 @@ export class DeploymentService extends Construct {
     const hostingBucket = new aws_s3.Bucket(this, "FrontendBucket", {
       blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL, // default to be accessible by anyone if not set
       removalPolicy: RemovalPolicy.DESTROY, // remove the bucket when the stack is destroyed
+      autoDeleteObjects: true
     });
 
     const distribution = new aws_cloudfront.Distribution(
@@ -47,11 +51,39 @@ export class DeploymentService extends Construct {
       }
     );
 
+    
+    // 创建自定义角色
+    const deploymentRole = new iam.Role(this, 'DeploymentRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    // 添加 S3 权限
+    deploymentRole.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        's3:GetObject*', 's3:GetBucket*', 's3:ListBucket',
+        's3:DeleteObject*', 's3:PutObject*', 's3:Abort*'
+      ],
+      resources: [hostingBucket.bucketArn, hostingBucket.arnForObjects('*')]
+    }));
+
+    // 添加 CloudFront 缓存失效权限
+    deploymentRole.addToPolicy(new iam.PolicyStatement({
+      actions: ['cloudfront:CreateInvalidation', 'cloudfront:GetInvalidation'],
+      resources: [distribution.distributionArn] // 限制到特定分配
+    }));
+
+    // 添加 Lambda 日志权限
+    deploymentRole.addToPolicy(new iam.PolicyStatement({
+      actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+      resources: ['*']
+    }));
+
     new aws_s3_deployment.BucketDeployment(this, "BucketDeployment", {
       sources: [aws_s3_deployment.Source.asset(path)], //read local files from the provided directory
       destinationBucket: hostingBucket, //specifies the S3 bucket where the assets will be uploaded.
       distribution, //tells AWS where our CloudFront distribution
       distributionPaths: ["/*"], //specifies the paths which CloudFront should invalidate once the new resources are pushed to the S3 bucket ( It's set to /* to indicate that all files in the distribution should be invalidated.)
+      role: deploymentRole // 指定自定义角色
     });
 
     new CfnOutput(this, "CloudFrontURL", {
